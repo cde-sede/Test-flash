756fed4826240746f8790b467b5682fe40cf88a4
cyteui <86572244+cyteui@users.noreply.github.com>
2023-03-09 18:45:55 +0100

    Update script-tv.js

--- js/script-tv.js
+++ js/script-tv.js
@@ -8,9 +8,9 @@ for (let index = 0; index < 12; index++) {
     card.style.opacity = 0
     card.innerHTML = `
         <a href="">
-            <div onmouseover="HoverStreamCard(this, 'ponce')" data-scroll data-scroll-speed="1" class="tv" id="p1">
+            <div onmouseover="HoverStreamCard(this, 'mistermv')" data-scroll data-scroll-speed="1" class="tv" id="p1">
                     <iframe src="" frameborder="0" allowfullscreen="true" scrolling="no" ></iframe>
-                    <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_gobgg.jpg" frameborder="0" allowfullscreen="true" scrolling="no" ></img>
+                    <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_mistermv.jpg" frameborder="0" allowfullscreen="true" scrolling="no" ></img>
             </div>
             <div class="icone">
                 <img class="icon" src="../image/test.jpg" alt="">
